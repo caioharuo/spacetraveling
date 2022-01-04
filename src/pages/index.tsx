09f01fb4b35b534258ae465e0d5b325d966e1cc6
caioharuo <caio.haruo@hotmail.com>
2022-01-04 18:45:36 -0300

    load and show data from prismic API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 import Link from 'next/link';
-import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
 
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import commonStyles from '../styles/common.module.scss';
+
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -26,90 +31,71 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ postsPagination }: HomeProps) {
   return (
     <div className={commonStyles.container}>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
       <Header />
-
       <main className={styles.posts}>
-        <Link href="#">
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div>
-              <time>
-                <img src="/images/calendar.svg" alt="ícone de calendário" />
-                15 Mar 2021
-              </time>
-              <span>
-                <img src="/images/user.svg" alt="ícone de usuário" />
-                Joseph Oliveira
-              </span>
-            </div>
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <strong>Criando um app CRA do zero</strong>
-            <p>
-              Tudo sobre como criar a sua primeira aplicação utilizando Create
-              React App
-            </p>
-            <div>
-              <time>
-                <img src="/images/calendar.svg" alt="ícone de calendário" />
-                19 Abr 2021
-              </time>
-              <span>
-                <img src="/images/user.svg" alt="ícone de usuário" />
-                Danilo Vieira
-              </span>
-            </div>
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div>
-              <time>
-                <img src="/images/calendar.svg" alt="ícone de calendário" />
-                15 Mar 2021
-              </time>
-              <span>
-                <img src="/images/user.svg" alt="ícone de usuário" />
-                Joseph Oliveira
-              </span>
-            </div>
-          </a>
-        </Link>
-        <Link href="#">
-          <a>
-            <strong>Criando um app CRA do zero</strong>
-            <p>
-              Tudo sobre como criar a sua primeira aplicação utilizando Create
-              React App
-            </p>
-            <div>
-              <time>
-                <img src="/images/calendar.svg" alt="ícone de calendário" />
-                19 Abr 2021
-              </time>
-              <span>
-                <img src="/images/user.svg" alt="ícone de usuário" />
-                Danilo Vieira
-              </span>
-            </div>
-          </a>
-        </Link>
+        {postsPagination.results.map(post => (
+          <Link key={post.uid} href={`/post/${post.uid}`}>
+            <a>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <time>
+                  <img src="/images/calendar.svg" alt="ícone de calendário" />
+                  {post.first_publication_date}
+                </time>
+                <span>
+                  <img src="/images/user.svg" alt="ícone de usuário" />
+                  {post.data.author}
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
       </main>
     </div>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 20,
+    }
+  );
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.last_publication_date),
+        'PP',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: { postsPagination },
+  };
+};
